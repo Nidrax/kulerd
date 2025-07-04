24ef4b3a1380efe37f89ac8ab977ac15f86466fb
Daniel Praźmo <d.prazmo@icloud.com>
2025-07-05 00:38:15 +0200

    Add header file

---
+++ kulerd.hpp
@@ -0,0 +1,81 @@
+#ifndef KULERD_HPP
+#define KULERD_HPP
+
+#include <cstdint>
+#include <string>
+#include <sstream>
+#include <map>
+
+namespace kulerd {
+    enum class Color {
+        Black,
+        Red,
+        Green,
+        Yellow,
+        Blue,
+        Magenta,
+        Cyan,
+        White,
+        BrightBlack,
+        BrightRed,
+        BrightGreen,
+        BrightYellow,
+        BrightBlue,
+        BrightMagenta,
+        BrightCyan,
+        BrightWhite,
+        Default
+    };
+
+    inline std::map<Color, uint8_t> Foreground = {
+        {Color::Black, 30},
+        {Color::Red, 31},
+        {Color::Green, 32},
+        {Color::Yellow, 33},
+        {Color::Blue, 34},
+        {Color::Magenta, 35},
+        {Color::Cyan, 36},
+        {Color::White, 37},
+        {Color::BrightBlack, 90},
+        {Color::BrightRed, 91},
+        {Color::BrightGreen, 92},
+        {Color::BrightYellow, 93},
+        {Color::BrightBlue, 94},
+        {Color::BrightMagenta, 95},
+        {Color::BrightCyan, 96},
+        {Color::BrightWhite, 97}
+    };
+
+    inline std::map<Color, uint8_t> Background = {
+        {Color::Black, 40},
+        {Color::Red, 41},
+        {Color::Green, 42},
+        {Color::Yellow, 43},
+        {Color::Blue, 44},
+        {Color::Magenta, 45},
+        {Color::Cyan, 46},
+        {Color::White, 47},
+        {Color::BrightBlack, 100},
+        {Color::BrightRed, 101},
+        {Color::BrightGreen, 102},
+        {Color::BrightYellow, 103},
+        {Color::BrightBlue, 104},
+        {Color::BrightMagenta, 105},
+        {Color::BrightCyan, 106},
+        {Color::BrightWhite, 107}
+    };
+
+    static std::string c(const std::string& text, const Color fgColor, const Color bgColor = Color::Default) {
+        std::stringstream ss;
+        if (fgColor == Color::Default && bgColor == Color::Default ||
+            !Foreground.contains(fgColor) || !Background.contains(bgColor)) {
+            return text;
+        }
+        const auto fg = fgColor == Color::Default ? Foreground.find(Color::White) : Foreground.find(fgColor);
+        const auto bg = bgColor == Color::Default ? Background.find(Color::Black) : Background.find(bgColor);
+        ss << "\033[" << static_cast<int>(fg->second) << ";" << static_cast<int>(bg->second) << "m" << text << "\033[0m";
+        return ss.str();
+    }
+} // namespace kulerd
+
+#endif //KULERD_HPP
